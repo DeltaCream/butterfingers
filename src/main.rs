231af1b3374e608db8e65f36d04e40cc5a295ee1
DeltaCream <cseanwyatt@gmail.com>
2023-10-05 21:32:44 +0800

    Initial commit containing all rust crate dependencies

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use libfprint_rs::{FpContext, FpPrint};
+
+fn main() {
+    // let context = FpContext::new();
+    // let devices = context.devices();
+
+    //Enrolling a new fingerprint
+    let context = FpContext::new();
+    let devices = context.devices();
+
+    let dev = devices.get(0).unwrap();
+    dev.open_sync(None).expect("Should work with no problems"); //?
+
+    let template = FpPrint::new(dev);
+    template.set_username("Bruce Banner");
+    
+    println!("{}", template.username().unwrap());
+
+    //Hello world for sanity check
+    println!("Hello, world!");
+    println!("Hello World");
+
+    //Verifying a fingerprint
+    let context = FpContext::new();
+    let devices = context.devices();
+
+    let dev = devices.get(0).unwrap();
+    dev.open_sync(None).expect("Should work here");
+
+    // let enrolled_print = load_print_from_file();
+
+    // let match_res = dev.verify_sync(enrolled_print, None, None, None::<()>, None).expect("Some error encountered while verifying the fingerprint."); //?
+}
