a0f89be75f8519866fe8bae7dc046230f908bf23
DeltaCream <cseanwyatt@gmail.com>
2023-10-05 22:51:42 +0800

    added expect messages to devices.get()
    to add descriptive error reporting

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,16 @@ fn main() {
     let context = FpContext::new();
     let devices = context.devices();
 
-    let dev = devices.get(0).unwrap();
+    let dev = devices.get(0).expect("No devices found");
     dev.open_sync(None).expect("Should work with no problems"); //?
 
     let template = FpPrint::new(dev);
     template.set_username("Bruce Banner");
     
     println!("{}", template.username().unwrap());
+
+
+    //
 
     //Hello world for sanity check
     println!("Hello, world!");
@@ -24,7 +27,7 @@ fn main() {
     let context = FpContext::new();
     let devices = context.devices();
 
-    let dev = devices.get(0).unwrap();
+    let dev = devices.get(0).expect("No devices found");
     dev.open_sync(None).expect("Should work here");
 
     // let enrolled_print = load_print_from_file();
