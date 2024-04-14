e28598d014647f0eb9a93a3b009d40ec5434ffcf
Walnuts <zed.walnuts.king@gmail.com>
2024-04-14 20:50:42 +0800

    frontend stuff

--- src/scanner.js
+++ src/scanner.js
@@ -1,9 +1,24 @@
 const { invoke } = window.__TAURI__.tauri;
 const { emit, listen } = window.__TAURI__.event;
 
+/* JSON STRUCTURE
+responsecode: "success" | "failure"
+body: {
+  id
+  fname
+  lname
+  time
+  date
+  code
+}
+ */
+
 let resultString = document.querySelector("#result-body");
+let btnIdentify = document.querySelector("#identify");
+let btnManual = document.querySelector("#manual");
 const revert = 7500; // time until text revert in ms
 
+// identify fingerprint
 async function start_identify() {
   try {
     const response = await invoke("start_identify");
@@ -12,16 +27,53 @@ async function start_identify() {
     // check if response has error key
     if (result && result.responsecode === "success") {
       const data = result.body;
-      resultString.textContent = JSON.stringify(data);
-      console.log("Response: ", JSON.stringify(data));
+      resultString.textContent = "<span class=\"success\">Attendance Recorded!</span>";
+
+      // change data
+      document.querySelector("#employee-image").src = "https://localhost/attendance/images/" + data[0];
+      document.querySelector("#employee").textContent = "<span class=\"success\">" + data[1] + " " + data[2] + "</span>";
+      document.querySelector("#employee-id").textContent = data[0];
+      document.querySelector("#date").textContent = data[4];
+      document.querySelector("#time").textContent = data[3];
+      if (data[5] === "in") {
+        document.querySelector("#code").textContent = "Time-In";
+      } else {
+        document.querySelector("#code").textContent = "Time-Out";
+      }
+
+      // popup employee window
+
+      console.log("Response: " + JSON.stringify(data));
 
       revertText();
     } else {
-      resultString.textContent = "<span class=\"error\">", result.body, "</span>";
-      console.error("Error in response: ", result.body);
+      resultString.textContent = "<span class=\"error\">" + result.body + "</span>";
+      console.error("Error in response: " + result.body);
 
       revertText();
     }
+  } catch (err) {
+    resultString.textContent = "<span class=\"error\">" + result.body + "</span>";
+    console.error("Error invoking start_identify: " + result.body);
+
+    revertText();
+  }
+}
+
+// manual attendance
+let inputID = document.querySelector("#emp_id");
+async function manual_attendance() {
+  try {
+    const emp_id = parseInt(inputID.value);
+    const response = await invoke("manual_attendance", { emp: emp_id });
+    const result = JSON.parse(response);
+
+    if (result && result.responsecode === "success") {
+
+    } else {
+
+    }
+
   } catch (err) {
     resultString.textContent = "<span class=\"error\">", result.body, "</span>";
     console.error("Error invoking start_identify: ", result.body);
@@ -30,11 +82,32 @@ async function start_identify() {
   }
 }
 
+// disable buttons and revert text back after a time
 function revertText() {
+  // disable buttons
+  btnIdentify.disabled = true;
+  btnManual.disabled = true;
+
   // revert text after [revert] ms
   setTimeout(() => {
     resultString.textContent = "Awaiting connection to scanner...";
+    btnIdentify.disabled = false;
+    btnManual.disabled = false;
   }, revert);
+}
+
+// popup
+function showPopup() {
+  const popup = document.querySelector("#popup");
+  popup.style.display = "block";
+  setTimeout(() => {
+    closePopup();
+  }, revert);
+}
+
+function closePopup() {
+  const popup = document.querySelector("#popup");
+  popup.style.display = "none";
 }
 
 /*
